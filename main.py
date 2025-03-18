39c178ed7f6912e50c1237680c8a659a27bc0d34
s-eash <eashwaransridhar@gmail.com>
2025-03-18 21:16:12 +0530

    Data Ingestion Added

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from src.CNNClassifier import logger
 
-logger.info("Welcome to CNN Classifier")
+from src.CNNClassifier import logger
+from src.CNNClassifier.pipeline.stage_01 import DataIngestionTrainingPipiline
+
+STAGE_NAME  = "Data Ingestion stage"
+
+
+try:
+    logger.info(f"Starting {STAGE_NAME}")
+    pipeline = DataIngestionTrainingPipiline()
+    pipeline.main()
+    logger.info(f"Completed {STAGE_NAME}")
+except Exception as e:
+    logger.error(f"Failed to run {STAGE_NAME} pipeline")
+    logger.error(e)
--- src/CNNClassifier/utils/common.py
+++ src/CNNClassifier/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from cnnClassifier import logger
+from CNNClassifier import logger
 import json
 import joblib
 from ensure import ensure_annotations
@@ -29,6 +29,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
+            print(content)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
